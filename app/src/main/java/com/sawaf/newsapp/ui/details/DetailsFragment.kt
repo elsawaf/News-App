8f7d5a94c20cc891821e3caaa98e77a1d5365371
Sawaf <Mohamed.Mostafa-ElSawaf@vodafone.com>
2023-06-18 18:23:03 +0300

    Details screen using SavedStateHandle.

--- app/src/main/java/com/sawaf/newsapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/sawaf/newsapp/ui/details/DetailsFragment.kt
@@ -7,34 +7,53 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.sawaf.newsapp.R
+import com.sawaf.newsapp.core.utils.observe
 import com.sawaf.newsapp.core.utils.toast
+import com.sawaf.newsapp.databinding.FragmentDetailsBinding
+import com.sawaf.newsapp.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailsFragment : Fragment() {
 
-    val args : DetailsFragmentArgs by navArgs()
-    private lateinit var viewModel: DetailsViewModel
+    private val viewModel by viewModels<DetailsViewModel>()
+
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        args.article?.also {
-            view.findViewById<TextView>(R.id.article_content).text = it.description
-            toast(it.title)
+        observe(viewModel.articleItem) { item ->
+            binding.apply {
+                articleContent.text = item.description
+                if (item.isBookmarked) {
+                    binding.saveBtn.setImageResource(android.R.drawable.btn_star_big_on)
+                } else {
+                    binding.saveBtn.setImageResource(android.R.drawable.btn_star_big_off)
+                }
+                saveBtn.setOnClickListener{
+                    viewModel.saveArticle(item)
+                }
+            }
+            toast(item.title)
         }
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(DetailsViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
--- app/src/main/java/com/sawaf/newsapp/ui/details/DetailsViewModel.kt
+++ app/src/main/java/com/sawaf/newsapp/ui/details/DetailsViewModel.kt
@@ -1,7 +1,31 @@
 package com.sawaf.newsapp.ui.details
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sawaf.newsapp.data.NewsRepoInterface
+import com.sawaf.newsapp.data.mapper.toEntity
+import com.sawaf.newsapp.ui.models.ArticleUi
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DetailsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class DetailsViewModel @Inject constructor(
+    private val newsRepoInterface: NewsRepoInterface,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+    val articleItem = MutableLiveData<ArticleUi>(savedStateHandle["article"])
+
+    fun saveArticle(articleUi: ArticleUi) {
+        viewModelScope.launch {
+            if (articleUi.isBookmarked) {
+                newsRepoInterface.removeArticle(articleUi.toEntity())
+            } else {
+                newsRepoInterface.saveArticle(articleUi.toEntity())
+            }
+            articleItem.value = articleUi.copy(isBookmarked = !articleUi.isBookmarked)
+        }
+    }
 }
